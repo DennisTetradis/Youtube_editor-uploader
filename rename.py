a77b17cc0fce515ba4a2d9cc1b50fa258a04d7e8
DionyshsTetradhs <dennis.tetradis@hotmail.com>
2023-01-09 15:51:41 +0200

    Create rename.py

---
+++ rename.py
@@ -0,0 +1,51 @@
+import os
+import random
+import time
+
+# Directory where the files are located
+directory = './Exports'
+
+# Get a list of all the files in the directory
+files = os.listdir(directory)
+
+# Shuffle the list of files
+random.shuffle(files)
+
+# Loop through the files
+for i, file in enumerate(files):
+    # Construct the new file name
+    new_name = 'files{}.mp4'.format(i + 1)
+
+    # Get the full path of the file
+    old_path = os.path.join(directory, file)
+
+    # Construct the full path of the new file name
+    new_path = os.path.join(directory, new_name)
+
+    # Rename the file
+    os.rename(old_path, new_path)
+
+time.sleep(1)
+
+# Directory where the files are located
+directory = './Exports'
+
+# Get a list of all the files in the directory
+files = os.listdir(directory)
+
+# Shuffle the list of files
+random.shuffle(files)
+
+# Loop through the files
+for i, file in enumerate(files):
+    # Construct the new file name
+    new_name = '{}.mp4'.format(i + 1)
+
+    # Get the full path of the file
+    old_path = os.path.join(directory, file)
+
+    # Construct the full path of the new file name
+    new_path = os.path.join(directory, new_name)
+
+    # Rename the file
+    os.rename(old_path, new_path)
