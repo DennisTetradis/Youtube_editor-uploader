e8e2c1bf17dc536019aab0956f30f6b5124d2f41
DionyshsTetradhs <dennis.tetradis@hotmail.com>
2023-01-15 19:29:30 +0200

    Update editor.py

--- editor.py
+++ editor.py
@@ -3,86 +3,43 @@ import os
 import random
 import time
 
-# Directory where the files are located
-directory = './Exports'
 
-# Get a list of all the files in the directory
-files = os.listdir(directory)
+def edit():
+    # load starting clip
+    starting_clip = VideoFileClip('videos/100.mp4')
 
-# Shuffle the list of files
-random.shuffle(files)
+    time = 0
+    count = 0
+    set_limit = 420 #Time of video!!!
+    print("Editing...")
+    while time < set_limit:
+        count += 1
+        clip = VideoFileClip('videos/{}.mp4'.format(count))
+        # Add Clip
+        if count == 1:
+            final = concatenate_videoclips([starting_clip, clip])
+        else:
+            final = concatenate_videoclips([final, clip])
 
-# Loop through the files
-for i, file in enumerate(files):
-    # Construct the new file name
-    new_name = 'files{}.mp4'.format(i + 1)
+        time = final.duration
 
-    # Get the full path of the file
-    old_path = os.path.join(directory, file)
 
-    # Construct the full path of the new file name
-    new_path = os.path.join(directory, new_name)
 
-    # Rename the file
-    os.rename(old_path, new_path)
+    # write
+    clip = VideoFileClip('outro/outro.mp4')
+    final = concatenate_videoclips([final, clip])
+    final.write_videofile('final/final{}.mp4'.format(len(os.listdir("./final"))+1))
 
-time.sleep(1)
-
-# Directory where the files are located
-directory = './Exports'
-
-# Get a list of all the files in the directory
-files = os.listdir(directory)
-
-# Shuffle the list of files
-random.shuffle(files)
-
-# Loop through the files
-for i, file in enumerate(files):
-    # Construct the new file name
-    new_name = '{}.mp4'.format(i + 1)
-
-    # Get the full path of the file
-    old_path = os.path.join(directory, file)
-
-    # Construct the full path of the new file name
-    new_path = os.path.join(directory, new_name)
-
-    # Rename the file
-    os.rename(old_path, new_path)
-
-# load starting clip
-starting_clip = VideoFileClip('Exports/60.mp4')
-
-time = 0
-count = 0
-set_limit = 420 #Time of video
-
-while time < set_limit:
-    count += 1
-    clip = VideoFileClip('Exports/{}.mp4'.format(count))
-    # Add Clip
-    if count == 1:
-        final = concatenate_videoclips([starting_clip, clip])
-    else:
-        final = concatenate_videoclips([final, clip])
-
-    time = final.duration
+    print('Edit complete!')
     print(time)
+    print('Deleting used videos...'+str(count))
+    y = 0
+    while y < count:
+        y += 1
+        if os.path.exists('videos/{}.mp4'.format(y)):
+            os.remove('videos/{}.mp4'.format(y))
+        print("Deleting:"+str(y))
 
-
-# write
-final.write_videofile('combined.mp4')
-
-print('Edit complete!'+str(count))
-
-y = 0
-while y < count:
-    y += 1
-    if os.path.exists('Exports/{}.mp4'.format(y)):
-        os.remove('Exports/{}.mp4'.format(y))
-    print("Deleting:"+str(y))
-
-if os.path.exists('Exports/60.mp4'):
-    os.remove('Exports/60.mp4')
-    print("Deleting:60")
+    if os.path.exists('videos/100.mp4'):
+        os.remove('videos/100.mp4')
+        print("Deleting:100")
